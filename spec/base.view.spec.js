7f0f6bdffa3ac6fd783a4d8fc1e17605e5f42988
eabait <estebanabait@gmail.com>
2013-10-12 15:43:16 -0300

    Esteban. BaseView complete spec suite

--- spec/base.view.spec.js
+++ spec/base.view.spec.js
@@ -38,11 +38,66 @@ describe('BaseView', function() {
 
   });
 
+  describe('Template management', function() {
+
+    it('should be able to fetch a template', function() {
+      expect(true).toBe(false);
+    });
+
+    it('should be able to compile a template', function() {
+      expect(true).toBe(false);
+    });
+
+    it('should return a compiled template when getTemplate is used', function() {
+      expect(true).toBe(false);
+    });
+
+    it('should cache templates', function() {
+      expect(true).toBe(false);
+    });
+
+  });
+
   describe('Rendering', function() {
-    it('renders itself', function() {
+    it('should render itself in the DOM', function() {
       $('body').append(view1.render().$el);
       expect(view1.el).toExist();
     });
+
+    it('should call beforeRender before render is invoked', function() {
+      expect(true).toBe(false);
+    });
+
+    it('should call afterRender after render is invoked', function() {
+      expect(true).toBe(false);
+    });
   });
 
+  describe('Loading', function() {
+    it('should call render once data has been fetched', function() {
+      expect(true).toBe(false);
+    });
+
+    it('should render a loading template while fetching data', function() {
+      expect(true).toBe(false);
+    });
+
+    it('should allow render be binded to different model events', function() {
+      expect(true).toBe(false);
+    });
+  });
+
+  describe('Disposal', function() {
+    it('should dispose itself', function() {
+      expect(true).toBe(false);
+    });
+
+    it('should not exist in the DOM after disposal', function() {
+      expect(true).toBe(false);
+    });
+
+    it('should not be binded to any event after disposal', function() {
+      expect(true).toBe(false);
+    });
+  });
 });
